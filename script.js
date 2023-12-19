88d1333267edb9c75b0cb1c9856a8032ae490925
ajzal-byte <ajzalaju10@gmail.com>
2023-12-19 07:09:26 +0530

    script updated

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 window.addEventListener('load',()=>{
+  const taskInput  = document.getElementById('task-input');
+  const submitButton = document.getElementById('task-submit');
+  const taskList = document.getElementById('tasks');
 
+  submitButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    if (taskInput.value == '') {
+      alert('Please enter something!');
+      return;
+    }
+    const newTasks = document.createElement('div');
+    newTasks.classList.add('task');
+    
+    const newTasks_content = document.createElement('div');
+    newTasks_content.classList.add('content');
+
+    const newTasks_input = document.createElement('input');
+    newTasks_input.type = 'text';
+    newTasks_input.classList.add('text')
+    newTasks_input.setAttribute('readonly')
+
+    newTasks.appendChild(newTasks_content);
+    taskList.appendChild(newTasks);
+  })
 })
-
-document.addEventListener('DOMContentLoaded', ()=>[
-  
-])
